66b74833573e6fd724d6dc6236e85811c7a275db
alex morales <alexander.morales35@my.unitec.edu.mx>
2025-02-04 11:38:29 -0600

    Update Practica 1 - 310125.cpp

--- punteros/Practica 1 - 310125.cpp
+++ punteros/Practica 1 - 310125.cpp
@@ -12,3 +12,30 @@ int main()
 
     return 0;
 }
+
+ //**Comprobar si un numero es par o impar, y señalar la posición de memoria donde se guarda el numero**
+int main()
+{
+    int numero, * dir_numero;
+
+    //Pedir numero al usuario
+    cout << "Digitel el numero: ";
+    cin >> numero;
+
+    //Guardando la posicio de memoria
+    dir_numero = &numero;
+    //Operador Ampersand (&) devuelve la dirección de memoria donde se almacena el valor de esa variable 
+
+    if (*dir_numero % 2 == 0)
+    {
+        cout << "El numero " << *dir_numero << " es par" << endl;
+        // El operador asterisco (*) accede al valor almacenado en la dirección de memoria que apunta a ese puntero
+        cout << "Poicion: " << dir_numero << endl;
+    }
+
+    else {
+        cout << "El numero " << *dir_numero << " es impar" << endl;
+        cout << "Posición: " << dir_numero << endl;
+    }
+    return 0;
+}
