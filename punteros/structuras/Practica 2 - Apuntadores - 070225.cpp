00a37825b4ccf482d4925d1bb8b0689ae7da8933
alex morales <alexander.morales35@my.unitec.edu.mx>
2025-02-07 12:06:50 -0600

    Update Practica 2 - Apuntadores - 070225.cpp

--- punteros/structuras/Practica 2 - Apuntadores - 070225.cpp
+++ punteros/structuras/Practica 2 - Apuntadores - 070225.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 using namespace std;
 
@@ -12,7 +11,6 @@ struct Arma
 
 int main()
 {
-    int opcion;
 
     //Paso 2: Definir las armas disponibles
     Arma armas[3] = /*[3] es para definir que habrá 3 elementos dentro del array, si tuvieramos 5 tipos de armas, el array sería armas[5]*/
@@ -20,7 +18,7 @@ int main()
         {"Espada oxidada", 25, 13}, {"Escudo magico", 20, 15}, {"Arco elfico", 50, 22}
     };
     int cantidadArmas = sizeof(armas) / sizeof(Arma);
-    
+
     //Paso 3: Definir el oro inicial del jugador
     int oroJugador = 49;
     int* oroActual = &oroJugador; //*oroActual accede al valor almacenado
@@ -28,66 +26,65 @@ int main()
 
     //Paso 4: Implementar el bucle inicial del juego
     //Crear un ciclo while para representar el ciclo principal del juego
-    int x = 1;
-    while (x < 2)
+    while (true)
     {
         //Mostrar un mensaje de bienvenida y el oro actual del jugador
         cout << "Has llegado a la tienda de armas ^^!" << endl;
         cout << "Cantidad de oro disponible: " << oroJugador << endl;
-        x++;
-    }
 
-    //Paso 5: Mostrar las armas disponibles
-    Arma *armasEnVenta = armas; // Almacenar el arreglo de armas en un puntero
-
-    //Recorrer el array armasEnVenta utilizando un bucle for
-    cout << endl; 
-    cout << "Las armas en venta son:" << endl;
-    for (int i = 0; i < cantidadArmas; i++)
-    {
-        cout << "Arma " << (i + 1) << ": " <<  armasEnVenta[i].nombre << endl;
-        cout << "Precio: " << armasEnVenta[i].precio << endl;
-        cout << "Damage: " << armasEnVenta[i].damage << endl;
+        //Paso 5: Mostrar las armas disponibles
+        Arma* armasEnVenta = armas; // Almacenar el arreglo de armas en un puntero
+        //Recorrer el array armasEnVenta utilizando un bucle for
         cout << endl;
-    }
-
-    //Paso 6: Mostrar el menú de opciones
-    cout << "Que deseas hacer ?" << endl;
-    cout << "1 - Comprar un arma" << endl;
-    cout << "2 - Salir de la tienda" << endl;
-    cout << "=> ";
-    cin >> opcion;
-
-    //Paso 7: Procesar la opcion seleccionada
-    //Utiliza la instruccion switch para procesar la opcion seleccionada
-    switch (opcion)
-    {
-    case 1:
-    //Menu de armas
-        cout << "Armas disponibles: " << endl;
+        cout << "Las armas en venta son:" << endl;
         for (int i = 0; i < cantidadArmas; i++)
         {
-            cout << "Arma " << (i + 1) << ": " << armasEnVenta[i].nombre << endl;
+            cout << i + 1 << "." << armasEnVenta[i].nombre << "Precio:" << armasEnVenta[i].precio << "Damage:" << armasEnVenta[i].damage << endl;
         }
-        cout << endl;
-        //Ingresar numero de arma
-        int nArma;
-        cout << "Que numero de arma deseas comprar ?" << endl;
+
+        //Paso 6: Mostrar el menú de opciones
+        cout << "Que deseas hacer ?" << endl;
+        cout << "1 - Comprar un arma" << endl;
+        cout << "2 - Salir de la tienda" << endl;
         cout << "=> ";
-        cin >> nArma;
-        //Validar que este en el rango
+        
+        int opcion;
+        cin >> opcion; 
 
-    case 2:
-     //Mensaje de despedida
-        cout << endl;
-        cout << "Gracias por visitar la tienda de armas." << endl;
-        cout << "Vuelve pronto !" << endl;
+        //Paso 7: Procesar la opcion seleccionada
+        //Utiliza la instruccion switch para procesar la opcion seleccionada
+        switch (opcion)
+        {
+        case 1:
+            //Menu de armas
+            cout << "Armas disponibles: " << endl;
+            for (int i = 0; i < cantidadArmas; i++)
+            {
+                cout << "Arma " << (i + 1) << ": " << armasEnVenta[i].nombre << endl;
+            }
+            cout << endl;
+            //Ingresar numero de arma
+            int nArma;
+            cout << "Que numero de arma deseas comprar ?" << endl;
+            cout << "=> ";
+            cin >> nArma;
+            //Validar que este en el rango
+            break;
 
-    default:
-        cout << endl;
-        cout << "La opcion ingresada es invalida ):" << endl;
+        case 2:
+            //Mensaje de despedida
+            cout << endl;
+            cout << "Gracias por visitar la tienda de armas." << endl;
+            cout << "Vuelve pronto !" << endl;
+            break;
+
+        default:
+            cout << endl;
+            cout << "La opcion ingresada es invalida ):" << endl;
+            break;
+        }
+
     }
-
-
+    
     return 0;
 }
