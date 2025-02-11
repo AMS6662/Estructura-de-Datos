db59167f266604daf68b45b1c7c79b2e3cae065a
alex morales <alexander.morales35@my.unitec.edu.mx>
2025-02-11 12:43:42 -0600

    Create Practica de clase - 110225.cpp

---
+++ Recursividad/Practica de clase - 110225.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+/*---------------------------- Repaso de apuntadores --------------------------------*/
+//PASO 1. Definir la estructura del personaje
+// Crea una estructura que contenga: nombre (string), clase (string), fuerza (int),
+// inteligencia (int), destreza (int)
+
+struct Personaje
+{
+	string nombre;
+	string clase;
+	int fuerza;
+	int inteligencia;
+	int destreza;
+};
+
+//PASO 2. Inicializar el personaje
+int main()
+{
+	Personaje personaje1;
+	cout << "Ingrese el nombre de su personaje: ";
+	cin >> personaje1.nombre;
+	cout << endl;
+
+	cout << "Ingrese la clase de su personaje (Guerero, Mago, Arquero): ";
+	cin >> personaje1.clase;
+	cout << endl;
+
+	//PASO 3. Asignar puntos de atributo
+	//El jugador empieza con 10 puntos de habilidad para distrubuir entra Fuerza, Inteligencia y Destreza
+	// Solicita al jugador que asigne estos puntos
+	//Verifica que no asigne más de 10 puntos en total
+	int puntosHabilidad = 10;
+	int habilidadTotal;
+	cout << "Su personaje tiene un total de 10 puntos de habilidad." << endl;
+	cout << "Puntos de Fuerza: ";
+	cin >> personaje1.fuerza;
+
+	cout << "Puntos de Inteligencia: ";
+	cin >> personaje1.inteligencia;
+
+	cout << "Puntos de Destreza: ";
+	cin >> personaje1.destreza;
+}
