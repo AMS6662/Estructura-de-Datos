9433f0a03a912041213a6d03fde94929a707dc61
alex morales <alexander.morales35@my.unitec.edu.mx>
2025-03-11 12:46:16 -0600

    Create E1.1 - Calculo XP 110325.cpp

---
+++ Practicario/E1.1 - Calculo XP 110325.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+//--------------------------- * Ejercicio 1.1: Calculadora de Puntos de Experiencia (XP) * ---------------------------------------
+
+/*Criterios de Evaluación:
+- Definición de la función CalcularXP correctamente. (10%)
+- Uso adecuado de parámetros y retorno de valores. (15%)
+- Cálculo correcto de XP (multiplicación por 100). (20%)
+- Uso adecuado de tipos de datos (int para la cantidad de enemigos y XP). (10%)
+- Pruebas con diferentes valores y output correcto en consola. (25%)
+- Código estructurado, legible y bien comentado. (20%)*/
+
+int enemigo = 1;
+int baseXP = 1;
+
+int main()
+{
+    cout << "Cuantos enemigos quieres atacar?"
+    return 0;
+}
+
+//Definicion de calcularXP
+void calcularXP(int enemigo, int baseXP) //Parametros
+{
+    baseXP = enemigo * 100;
+}
